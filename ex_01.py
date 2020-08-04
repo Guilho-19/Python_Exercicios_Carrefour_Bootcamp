b60e621cd260fba2dbc193487476457e9b20c92f
Guilherme Palaro <68549604+Gulho-19@users.noreply.github.com>
2020-08-04 12:00:08 -0300

    Add files via upload

---
+++ ex_01.py
@@ -0,0 +1,10 @@
+print('=====================================')
+print('Tabela de Multiplicações do número 9')
+print('=====================================')
+numero = 0
+limite = 10
+while numero <= limite:
+    resultado = numero * 9
+    print('9 x ' + str(numero) + ': ' + str(resultado))
+    numero = numero + 1
+print('=====================================')
